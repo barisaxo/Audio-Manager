3c87d351357c2a2f830b3e8fb4d849a3ba73316e
Bee <59021624+barisaxo@users.noreply.github.com>
2020-05-26 20:00:40 -0700

    Add files via upload

---
+++ AudioAssetSub.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioAssetSub : MonoBehaviour
+{
+    public AudioManager audioManager;
+    public AudioClip[] musicClip = new AudioClip[1];
+    public AudioClip[] ambienceClip = new AudioClip[1];
+    public AudioClip[] localSFXClip = new AudioClip[1];
+    public AudioClip[] worldSFXClip = new AudioClip[1];
+
+    void Start()
+    {
+        //load audio from the Resources folder here
+        //musicClip[0] = Resources.Load<AudioClip>("AUDIO/Music/*file name*");
+        //worldSFXClip[0] = Resources.Load<AudioClip>("AUDIO/WorldSFX/*file name*");
+        //ambienceClip[0] = Resources.Load<AudioClip>("AUDIO/Ambience/*file name");
+        //localSFXClip[0] = Resources.Load<AudioClip>("AUDIO/LocalSFX/*file name*");
+    }
+}
---
+++ LocalSfxAS.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalSfxAS : MonoBehaviour
+{
+    public AudioManager audioManager;
+    public AudioSource localSFX;
+    public AudioClip[] localSFXClip;
+
+    public float localSFXClipLength, timePosition;
+
+    [Range(0f, 1f)]
+    public float volume;
+
+    void Start()
+    {
+        localSFX = gameObject.AddComponent<AudioSource>();
+        localSFX.volume = volume;
+        localSFXClip = audioManager.audioAssetSub.localSFXClip;
+    }
+
+    public void LoadLocalSFXClip(int index)
+    {
+        localSFX.clip = localSFXClip[index];
+    }
+
+    public void FindLocalSFXClipLength()
+    {
+        localSFXClipLength = localSFX.clip.length;
+    }
+
+    public void PlayLocalSFX()
+    {
+        localSFX.Play();
+    }
+
+    public void PlayLocalSFXFromTime(float fromSeconds)
+    {
+        localSFX.time = fromSeconds;
+        localSFX.Play();
+    }
+
+    public void StopLocalSFX()
+    {
+        localSFX.Stop();
+    }
+
+    public void PauseLocalSFX()
+    {
+        localSFX.Pause();
+    }
+
+    public void UnPauseLocalSFX()
+    {
+        localSFX.UnPause();
+    }
+
+    public void SetCurrentLocalSFXTimePosition()
+    {//to set/store a playback position
+        timePosition = localSFX.time;
+    }
+
+    public void PlayAmbienceFromUntilTime(float fromSeconds, float untilSeconds)
+    {
+        localSFX.time = fromSeconds;
+        localSFX.Play();
+        StartCoroutine(StopAtTime(untilSeconds));
+    }
+
+    public void PlayLocalSFXUntilTime(float atSeconds)
+    {
+        StartCoroutine(StopAtTime(atSeconds));
+    }
+
+    IEnumerator StopAtTime(float seconds)
+    {
+        while (localSFX.time < seconds)
+        {
+            yield return new WaitForSeconds(seconds - localSFX.time);
+        }
+        localSFX.Stop();
+        yield return null;
+    }
+
+    //private void Update()
+    //{
+    //    Debug.Log(localSFX.time);
+    //}
+
+    public void FadeOutLocalSFX(float fade)
+    {//do not use fadeout while loop == true;
+        FindLocalSFXClipLength();
+        StartCoroutine(FadingLocalSFXOut(fade));
+    }
+
+    IEnumerator FadingLocalSFXOut(float fade)
+    {
+        while (localSFX.time < localSFXClipLength)
+        {
+            if (localSFX.time >= localSFXClipLength - 1.5f)
+            {
+                localSFX.volume -= localSFX.volume * Time.deltaTime * fade;
+            }
+            yield return new WaitForEndOfFrame();
+        }
+        localSFX.volume = volume;
+        yield return null;
+    }
+
+    public void FadeInLocalSFX(float fade)
+    {
+        localSFX.volume = .0f;
+        StartCoroutine(FadingLocalSFXIn(fade));
+    }
+
+    IEnumerator FadingLocalSFXIn(float fade)
+    {
+        while (localSFX.volume < volume)
+        {
+            localSFX.volume += Time.deltaTime * fade;
+            yield return new WaitForEndOfFrame();
+        }
+        localSFX.volume = volume;
+        yield return null;
+    }
+}
